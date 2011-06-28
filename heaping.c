31e124b79c2d847a4406754a4f5b007e2f3cd903
Abhijit Menon-Sen <ams@toroid.org>
2011-06-28 12:02:19 +0530

    Add #include <netinet/in_systm.h>
    The compile fails on FreeBSD without this, because it uses u_int, n_long
    etc. in netinet/ip.h. How very annoying.

--- heaping.c
+++ heaping.c
@@ -16,6 +16,7 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in_systm.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
